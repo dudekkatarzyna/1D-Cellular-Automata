7684e246dd3527eb13df7d7b7bc2565fb011c4f2
Katarzyna Dudek <dudekatarzyna7@gmail.com>
2019-05-03 19:37:23 +0200

    fixed mesh

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
@@ -25,8 +26,8 @@ def updateMesh():
 
     wid.canvas.clear()
 
-    height = 550
-    width = 500
+    height = Window.height
+    width = Window.width
 
     stepWidth = floor(width / (size + 1))
     stepHeight = floor(height / (size + 1))
@@ -36,17 +37,17 @@ def updateMesh():
         for index in range(0, iteration + 1):
             # poziome
             Line(points=[0.1 * width,
-                         stepHeight * (size - index + 0.2 * (size + 1)),
+                         stepHeight*(size-index),
                          size * stepWidth + 0.1 * width,
-                         stepHeight * (size - index + 0.2 * (size + 1))],
+                         stepHeight * (size-index)],
                  width=1)
 
         for index in range(0, size + 1):
             # pionowe
             Line(points=[(size - index) * stepWidth + 0.1 * width,
-                         size + ((size - iteration) * stepHeight),
+                         stepHeight*size,
                          (size - index) * stepWidth + 0.1 * width,
-                         stepHeight * (size + 0.2 * (size + 1))],
+                         stepHeight*(size-iteration)],
                  width=1)
 
     pass
@@ -120,60 +121,81 @@ def reset(self):
     pass
 
 
-class CellularAutomatonApp(App):
+class NewClass(FloatLayout):
 
-    def build(self):
+    def __init__(self, **kwargs):
+        super(NewClass, self).__init__(**kwargs)
+
         Window.size = (600, 680)
 
-        layout.add_widget(Label(text='Starting point:', size_hint_x=None, width=100, size_hint=(.2, .08),
-                                pos_hint={'x': 0.05, 'y': 1.9}))
+        self.add_widget(Label(text='Starting point:', size_hint_x=None, width=100, size_hint=(.2, .08),
+                              pos_hint={'x': 0.05, 'y': 1.9}))
         self.startingPoint = TextInput(size_hint_x=None, width=30, multiline=False, size_hint=(.09, .08),
                                        pos_hint={'x': .25, 'y': 1.9}, input_filter='int')
         self.startingPoint.bind(on_text_validate=on_enter_starting_point)
-        layout.add_widget(self.startingPoint)
+        self.add_widget(self.startingPoint)
 
         resetButton = Button(text='RESET', size_hint_x=None, width=100, size_hint=(.2, .08),
                              background_color=(1, 0, 0, 1), pos_hint={'x': 0.7, 'y': 1.9})
         resetButton.bind(on_press=reset)
-        layout.add_widget(resetButton)
+        self.add_widget(resetButton)
 
-        layout.add_widget(Label(text='Enter size:', size_hint_x=None, width=100, size_hint=(.2, .1),
-                                pos_hint={'x': 0.05, 'y': 0.1}))
+        self.add_widget(Label(text='Enter size:', size_hint_x=None, width=100, size_hint=(.2, .1),
+                              pos_hint={'x': 0.05, 'y': 0.1}))
 
         sizeInput = TextInput(size_hint_x=None, width=50, multiline=False, size_hint=(.2, .1),
                               pos_hint={'x': .25, 'y': .1}, input_filter='int')
         sizeInput.bind(on_text_validate=on_enter_size)
 
-        layout.add_widget(sizeInput)
-        layout.add_widget(Label(text='Rule:', size_hint_x=None, width=100, size_hint=(.2, .1),
-                                pos_hint={'x': .40, 'y': .1}))
+        self.add_widget(sizeInput)
+        self.add_widget(Label(text='Rule:', size_hint_x=None, width=100, size_hint=(.2, .1),
+                              pos_hint={'x': .40, 'y': .1}))
 
         ruleInput = TextInput(size_hint_x=None, width=50, multiline=False, size_hint=(.2, .1),
                               pos_hint={'x': .55, 'y': .1}, input_filter='int')
         ruleInput.bind(on_text_validate=rule_input)
-        layout.add_widget(ruleInput)
+        self.add_widget(ruleInput)
 
-        layout.add_widget(Label(text='Iteration No.:', size_hint_x=None, width=100, size_hint=(.2, .1),
-                                pos_hint={'x': .33, 'y': 1.89}))
+        self.add_widget(Label(text='Iteration No.:', size_hint_x=None, width=100, size_hint=(.2, .1),
+                              pos_hint={'x': .33, 'y': 1.89}))
 
         iterationInput = TextInput(size_hint_x=None, width=30, multiline=False, size_hint=(.1, .1),
                                    pos_hint={'x': .5, 'y': 1.89}, input_filter='int')
         iterationInput.bind(on_text_validate=iteration_input)
-        layout.add_widget(iterationInput)
+        self.add_widget(iterationInput)
 
         calculateButton = Button(text='Calculate', size_hint_x=None, width=100, size_hint=(.2, .1),
                                  pos_hint={'x': 0.75, 'y': 0.1})
-        calculateButton.bind(on_press=calculateAction)
-        layout.add_widget(calculateButton)
+        calculateButton.bind(on_press=self.tmp)
+        self.add_widget(calculateButton)
 
+
+    def tmp(self, *args):
+        global width, height
+
+
+        with self.canvas.before:
+            Color(1, 0, 0, 1)
+            Rectangle(pos=self.pos,
+                      size=self.size)
+
+        print(self.width)
+        print(self.height)
+
+        print(width)
+        print(height)
+
+
+class CellularAutomatonApp(App):
+
+    def build(self):
         root = BoxLayout(orientation='vertical')
         root.add_widget(wid)
-        root.add_widget(layout)
-
+        root.add_widget(NewClass())
         return root
 
 
 if __name__ == '__main__':
-    layout = FloatLayout(size=(600, 600))
+    # self = FloatLayout(size=(600, 600))
     wid = Widget()
     CellularAutomatonApp().run()
